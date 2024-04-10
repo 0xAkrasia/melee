36b2ac7b6cf0c64fcddb3edfe4cc4de7d815f9fc
0xAkrasia <115685790+0xAkrasia@users.noreply.github.com>
2024-04-10 11:02:25 -0700

    privy wallet connection

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import './styles'
-import './scripts'
-import IndexView from './views/IndexView';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import {PrivyProvider} from '@privy-io/react-auth';
+import { defineChain } from 'viem'
+// let privy_key = import.meta.env.VITE_PRIVY_KEY;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <IndexView />
-);
+export const inco = defineChain({
+  id: 9090,
+  name: 'Inco Gentry Testnet',
+  network: 'Inco Gentry Testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'INCO',
+    symbol: 'INCO',
+  },
+  rpcUrls: {
+    default: {
+        http: ['https://testnet.inco.org']
+    },
+    public: {
+        http: ['https://testnet.inco.org']
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://explorer.testnet.inco.org/'},
+  },
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PrivyProvider
+      appId={"clsay39yw04tv13s57t7fig9f"}
+      config={{
+          loginMethods: ['email', 'google', 'discord', 'wallet'],
+          defaultChain: inco,
+          supportedChains: [inco],
+          appearance: {
+              theme: 'dark',
+              accentColor: '#3673f5',
+              logo: 'https://raw.githubusercontent.com/0xAkrasia/In-Theory-Games/main/src/images/sculpture_no_art.png',
+              showWalletLoginFirst: false,
+              walletList: ['coinbase_wallet', 'wallet_connect', 'metamask'],
+          },
+      }}
+    >
+      <App />
+    </PrivyProvider>
+  </React.StrictMode>
+)
