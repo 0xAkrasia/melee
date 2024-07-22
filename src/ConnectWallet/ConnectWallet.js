d80a8e143e692c7e4729fc1545e4071a6cd4b353
Usama Latif <usamalatif25@gmail.com>
2024-07-23 00:04:51 +0500

    connect wallet and Glow text

--- src/ConnectWallet/ConnectWallet.js
+++ src/ConnectWallet/ConnectWallet.js
@@ -1,38 +1,38 @@
-import { usePrivy, useWallets } from '@privy-io/react-auth';
+import { usePrivy, useWallets } from "@privy-io/react-auth";
 
 export const LoginButton = ({ authenticated }) => {
-    const { login, logout } = usePrivy();
-    const { wallets } = useWallets();
+	const { login, logout } = usePrivy();
+	const { wallets } = useWallets();
 
-    let address = '';
-    try {
-        if (wallets.length > 0) {
-            address = wallets[0].address.slice(0, 4) + '...' + wallets[0].address.slice(-4);
-        }
-    } catch (e) {
-        console.log(e)
-    };
+	let address = "";
+	try {
+		if (wallets.length > 0) {
+			address =
+				wallets[0].address.slice(0, 4) + "..." + wallets[0].address.slice(-4);
+		}
+	} catch (e) {
+		console.log(e);
+	}
 
-    if (authenticated) {
-        return (
-            <button 
-                className="connect-wallet-button connect-wallet-text"
-                onClick={logout}
-            >
-                { address }
-            </button>
-        );
-    } else {
-        return (
-            <button 
-                className="connect-wallet-button connect-wallet-text "
-                onClick={login}
-            >
-                Connect Wallet
-            </button>
-        );
-    }
+	if (authenticated) {
+		return (
+			<button
+				className="connect-wallet-button connect-wallet-text"
+				onClick={logout}
+			>
+				{address}
+			</button>
+		);
+	} else {
+		return (
+			<button
+				className="connect-wallet-button connect-wallet-text "
+				onClick={login}
+			>
+				Connect Wallet
+			</button>
+		);
+	}
 };
-
 
 // text-shadow: 0 0 12px #ff8700;
--- src/keynesianBeautyContest/betInput.js
+++ src/keynesianBeautyContest/betInput.js
@@ -3,25 +3,27 @@ import "../css/betInput.css"; // Ensure the CSS file exists and is imported
 import "@fortawesome/fontawesome-free/css/all.min.css"; // Import Font Awesome CSS
 
 const BetInput = ({ value, onValueChange, maxValue, onMaxClick }) => {
-  return (
-    <div className="bet-input-wrapper">
-      <div className="bet-input-container">
-        <div className="bet-input-icon">
-          <i className="fab fa-ethereum"></i> {/* Ethereum icon */}
-        </div>
-        <input
-          type="text"
-          className="bet-input"
-          value={value}
-          onChange={onValueChange}
-        />
-        <div className="bet-input-max">
-          <span>{maxValue}</span>
-          <button className="max-button" onClick={onMaxClick}>Max</button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="bet-input-wrapper">
+			<div className="bet-input-container">
+				<div className="bet-input-icon">
+					<i className="fab fa-ethereum"></i> {/* Ethereum icon */}
+				</div>
+				<input
+					type="text"
+					className="bet-input"
+					value={value}
+					onChange={onValueChange}
+				/>
+				<div className="bet-input-max">
+					<span>{maxValue}</span>
+					<button className="max-button" onClick={onMaxClick}>
+						Max
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default BetInput;
